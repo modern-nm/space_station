f2fb84eddab281ddfa98126289d621dd4c576700
MODERN <87994977+modern-nm@users.noreply.github.com>
2024-04-19 14:38:48 +0300

    hitchSystem

---
+++ Content.Client/_Rat/HitchSystem.cs
@@ -0,0 +1,17 @@
+using Content.Shared._Rat.Hitch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Client._Rat.Hitch
+{
+    public sealed class HitchSystem : SharedHitchSystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
---
+++ Content.Shared/_Rat/HitchComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.Damage;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Rat.Hitch
+{
+    [RegisterComponent, NetworkedComponent]
+    public sealed partial class HitchComponent : Component
+    {
+    }
+}
